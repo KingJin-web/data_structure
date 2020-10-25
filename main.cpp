4ab64d67414f5dc20ef6c3b54222cd2539153c34
12613 <1261337209@qq.com>
2020-10-25 10:09:39 +0800

    .

--- main.cpp
+++ main.cpp
@@ -30,24 +30,24 @@ using namespace std;
 // 在任意一个四位整数中，如果该数的个位数和百位数之和大于16，并且十位数和千位数之和小于3，就称这种数为“老九幸运数“，
 // 请小伙伴编程计算出在1000~9999中，一共有多少个满足老九幸运数的四位整数呢，打印出最终的个数
 
-int main()
-{
-    int a, b, c, d, co = 0;
-    for (int i = 1000; i <= 9999; i++)
-    {
+// int main()
+// {
+//     int a, b, c, d, co = 0;
+//     for (int i = 1000; i <= 9999; i++)
+//     {
 
-        a = i % 10;
-        b = i / 10 % 10;
-        c = i / 100 % 10;
-        d = i / 1000;
-        if (a + c > 16 && b + d < 3)
-        {
-            cout<<i<<endl;
-            co ++;
-        }
-    }
-    cout<<"一共有 "<<co<<" 个"<<endl;
-}
+//         a = i % 10;
+//         b = i / 10 % 10;
+//         c = i / 100 % 10;
+//         d = i / 1000;
+//         if (a + c > 16 && b + d < 3)
+//         {
+//             cout<<i<<endl;
+//             co ++;
+//         }
+//     }
+//     cout<<"一共有 "<<co<<" 个"<<endl;
+// }
 
 // int main()
 // {
@@ -60,3 +60,39 @@ int main()
 //     d = num / 1000;
 //     cout << a << b << c << d << endl;
 // }
+
+// #define N 5 //宏定义 ，并不是必须的  好处是功能方便
+// int main()
+// {
+//     int nums[N] = {16, 25, 9, 90, 23};
+//     int i, j;
+//     int temp;
+//     // 先确定外层循环
+//     for (i = 0; i < N - 1; i++)
+//     {
+//         for (j = 0; j < N - i - 1; j++)
+//         {
+//             if (nums[j] < nums[j + 1])
+//             {
+//                 temp = nums[j];
+//                 nums[j] = nums[j + 1];
+//                 nums[j + 1] = temp;
+//             }
+//         }
+//     }
+//     printf("排序后的数组是：\n");
+//     for (i = 0; i < N; i++)
+//     {
+//         printf("%d\t", nums[i]);
+//     }
+// }
+
+
+
+int main()
+{
+    int a,b,c;
+    a = 72, b = 73, c = 74;
+    printf("%f%f%f\n", a, b, c);
+    return 0;
+}
