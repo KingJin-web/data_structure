e70967cfc68ca45bf6cda406a308b1cd775f72b1
12613 <1261337209@qq.com>
2021-03-29 01:14:46 +0800

    递归

---
+++ Pta/First/e.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define N 100
+
+int fmax(int a[], int i);
+
+int main()
+{
+    int A[N], n, i;
+    cin >> n;
+    for (i = 0; i < n; i++)
+        cin >> A[i];
+    int x = fmax(A, n);
+    cout << x;
+    return 0;
+}
+
+int fmax(int a[], int i){
+    int max = a[0];
+    for (int j = 0; j < i; i++)
+    {
+        if (max < a[j])
+        {
+            max = a[j];
+        }else
+        {
+            break;
+        }
+        
+        
+    }
+
+    return max;
+    
+}
+
+/* 请在这里填写答案 */
