91f3a7104374681e6cadce59167e8fa0889bd362
12613 <1261337209@qq.com>
2021-03-29 01:05:36 +0800

    6-5 数组最大元素值（递归法） (10 分)

---
+++ 算法Pta/第一次递归/d.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#define OK 1
+#define ERROR 0
+typedef int ElemType;
+using namespace std;
+
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode *next;
+} LNode, *LinkList;
+
+void CreateList(LinkList &L, int n)
+{
+    LinkList p;
+    int length = 1;
+    L = new LNode;
+    cin >> L->data;
+    L->next = NULL;
+    while (length < n)
+    {
+        p = new LNode;
+        cin >> p->data;
+        p->next = L->next;
+        L->next = p;
+        length++;
+    }
+}
+
+int Sum(LinkList L);
+
+int main()
+{
+    LinkList L;
+    ElemType e;
+    int length;
+    cin >> length;
+    CreateList(L, length);
+    int x = Sum(L);
+    cout << x;
+    return 0;
+}
+
+/* 请在这里填写答案 */
+
+//求以h为头指针的单链表的节点个数
+
+// int Sum(LinkList L)
+// {
+//     if (L == NULL)
+//         return 0;
+//     else
+//         return 1 + Sum(L->next);
+// }
+//反向显示以h为头指针的单链表的所有节点值
