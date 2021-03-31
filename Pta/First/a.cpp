f3d7dafce930338efe5f4bd6c122c543d5706a50
12613 <1261337209@qq.com>
2021-03-31 16:39:13 +0800

    Create_Sq

--- Pta/First/a.cpp
+++ Pta/First/a.cpp
@@ -14,6 +14,17 @@ typedef struct
 void Create_Sq(SqList &L); //建立待排序列，实现细节隐藏
 void Merge(SqList L, int low, int m, int high);
 
+void Create_Sq(SqList &L)
+{
+    int i, n;
+    cin >> n; //输入的值不大于 MAXSIZE
+    for (i = 1; i <= n; i++)
+    {
+        cin >> L.r[i];
+        L.length++;
+    }
+}
+
 void MSort(SqList L, int low, int high)
 { /*用分治法进行二路归并排序*/
     int mid;
@@ -30,6 +41,7 @@ int main()
 {
     SqList L;
     int i;
+ 
     L.r = new ElemType[MAXSIZE + 1];
     L.length = 0;
     Create_Sq(L);
@@ -44,23 +56,48 @@ int main()
 }
 /* 请在这里填写答案 */
 
-
+// void Merge(SqList L, int low, int m, int high)
+// {
+//     int b[1001];
+//     int p1 = low, p2 = m + 1;
+//     for (int i = low; i <= high; i++)
+//     {
+//         if ((p1 <= m) && ((p2 > high) || (L.r[p1] < L.r[p2])))
+//         {
+//             b[i] = L.r[p1++];
+//         }
+//         else
+//         {
+//             b[i] = L.r[p2++];
+//         }
+//     }
+//     for (int i = low; i <= high; i++)
+//         L.r[i] = b[i];
+// }
 void Merge(SqList L, int low, int m, int high)
 {
-    int b[1001];
-    int p1 = low, p2 = m + 1;
-    for (int i = low; i <= high; i++)
-    {
-        if ((p1 <= m) && ((p2 > high) || (L.r[p1] < L.r[p2])))
-        {
-            b[i] = L.r[p1++];
-        }
-        else
-        {
-            b[i] = L.r[p2++];
-        }
-    }
-    for (int i = low; i <= high; i++)
-        L.r[i] = b[i];
-}
+    //将有序表a[low..m]和a[m+1..high]归并为有序表r[low..high]
+    int i, j, k;
+    i = low;
+    j = m + 1;
+    k = 0;
+    int *a;
+    a = L.r;
+    int *r = new int[high - low + 1];
 
+    while (i <= m && j <= high)
+    {
+        //将a中记录由小到大地并入r中
+        if (a[i] <= a[j])
+            r[k++] = a[i++];
+        else
+            r[k++] = a[j++];
+    }
+    while (i <= m) //将剩余的a[low..m]复制到r中
+        r[k++] = a[i++];
+    while (j <= high) //将剩余的a[j.high]复制到r中
+        r[k++] = a[j++];
+    for (k = 0, i = low; i <= high; k++, i++)
+        a[i] = r[k];
+    delete[] r;
+} //Merge
