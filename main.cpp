72e7dcf2042acfab922f3272f810d108a6be2ebc
12613 <1261337209@qq.com>
2021-07-05 22:59:42 +0800

    ✔✔

--- main.cpp
+++ main.cpp
@@ -1900,107 +1900,129 @@ using namespace std;
 //     return 0;
 // }
 
-int strlen(char *str)
-{
-    string s = str;
-    return s.length();
-}
-// int strlen2(char *str)
+// int strlen(char *str)
 // {
+//     string s = str;
+//     return s.length();
+// }
+// // int strlen2(char *str)
+// // {
 
-//     return sizeof(str) / sizeof(char);
+// //     return sizeof(str) / sizeof(char);
+// // }
+
+// // int main(int argc, char const *argv[])
+// // {
+// //     cout<<strlen2("123456789");
+// //     return 0;
+// // }
+// // #include <stdlib.h>
+// // #include <stdio.h>
+// // void jiuzhe()
+// // {
+// //     int i;
+// //     double *ptr;
+// //     ptr = (double *)malloc(5 * sizeof(double));
+// //     for (i = 0; i < 3; i++)
+// //     {
+// //         scanf("%lf", &ptr[i]);
+// //     }
+// //     for (i = 0; i < 3; i++)
+// //     {
+// //         printf("%lf", ptr[i]);
+// //         /* code */
+// //     }
+
+// //     printf("jiuzhe");
+// // }
+// // int main()
+// // {
+// //     printf("jiuzhea!");
+// //     jiuzhe();
+// // }
+
+// //定义一个结构体
+
+// #include <stdio.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #define MAX 100 
+// typedef struct Student
+// {
+//     int stu_id;
+//     char name[100];
+// } Stu;
+
+// Stu stuw[2] = {
+//     {20180001, "xiaoming"},
+//     {20180002, "小明"}};
+
+// void write()
+// {
+//     // 定义一个文件指针
+//     FILE *fp;
+
+//     // 初始化一个结构体数组
+
+//     // 打开文件，没有文件自动创建
+//     fp = fopen("student.dat", "wb");          // b:表示以二进制写入
+//                                               // 写入数据
+//     fwrite((char *)stuw, sizeof(Stu), 2, fp); //2：表示将数组中两个元素写入文件
+//     // 关闭文件
+//     fclose(fp);
 // }
 
+// void read()
+// {
+//     // 定义一个文件指针
+//     FILE *fp;
+//     // 定义一个buf结构体，用于得到文件内容
+//     struct stat buf;
+//     // 定义一个文件行数记录变量
+//     int rows;
+//     // 定义一个Student结构体
+//     Stu stur[10];
+
+//     // 求文件中的行数（记录个数）
+//     stat("student.dat", &buf);
+//     rows = buf.st_size / sizeof(Stu);
+
+//     // 打开文件
+//     fp = fopen("student.dat", "rb");
+//     // 读取数据到数组中
+//     fread((char *)stur, sizeof(Stu), rows, fp);
+//     // 关闭文件
+//     fclose(fp);
+
+//     // 遍历数组，打印数据信息
+//     for (int i = 0; i < rows; i++)
+//         printf("%d\t%s\n", stur[i].stu_id, stur[i].name);
+// }
 // int main(int argc, char const *argv[])
 // {
-//     cout<<strlen2("123456789");
+//     write();
+//     read();
 //     return 0;
 // }
-// #include <stdlib.h>
-// #include <stdio.h>
-// void jiuzhe()
-// {
-//     int i;
-//     double *ptr;
-//     ptr = (double *)malloc(5 * sizeof(double));
-//     for (i = 0; i < 3; i++)
-//     {
-//         scanf("%lf", &ptr[i]);
-//     }
-//     for (i = 0; i < 3; i++)
-//     {
-//         printf("%lf", ptr[i]);
-//         /* code */
-//     }
 
-//     printf("jiuzhe");
-// }
-// int main()
-// {
-//     printf("jiuzhea!");
-//     jiuzhe();
-// }
-
-//定义一个结构体
-
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#define MAX 100 
-typedef struct Student
-{
-    int stu_id;
-    char name[100];
-} Stu;
-
-Stu stuw[2] = {
-    {20180001, "xiaoming"},
-    {20180002, "小明"}};
-
-void write()
-{
-    // 定义一个文件指针
-    FILE *fp;
-
-    // 初始化一个结构体数组
-
-    // 打开文件，没有文件自动创建
-    fp = fopen("student.dat", "wb");          // b:表示以二进制写入
-                                              // 写入数据
-    fwrite((char *)stuw, sizeof(Stu), 2, fp); //2：表示将数组中两个元素写入文件
-    // 关闭文件
-    fclose(fp);
-}
-
-void read()
-{
-    // 定义一个文件指针
-    FILE *fp;
-    // 定义一个buf结构体，用于得到文件内容
-    struct stat buf;
-    // 定义一个文件行数记录变量
-    int rows;
-    // 定义一个Student结构体
-    Stu stur[10];
-
-    // 求文件中的行数（记录个数）
-    stat("student.dat", &buf);
-    rows = buf.st_size / sizeof(Stu);
-
-    // 打开文件
-    fp = fopen("student.dat", "rb");
-    // 读取数据到数组中
-    fread((char *)stur, sizeof(Stu), rows, fp);
-    // 关闭文件
-    fclose(fp);
-
-    // 遍历数组，打印数据信息
-    for (int i = 0; i < rows; i++)
-        printf("%d\t%s\n", stur[i].stu_id, stur[i].name);
-}
 int main(int argc, char const *argv[])
 {
-    write();
-    read();
+    int n, m, i, j;
+    char x, s[20];
+    while (scanf("%s", s) != EOF)
+    {
+
+        if (strlen(s) != 1)
+        {
+            printf("输入错误,请重新输入\n");
+            continue;
+        }
+        m = s[0] - '0';
+        if (m == 1)
+            printf("%d\n", n);
+        else
+            printf("输入错误,请重新输入\n");
+    }
+
     return 0;
 }
