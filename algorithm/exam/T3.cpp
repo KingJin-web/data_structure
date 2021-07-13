fc9e10896d6c1111ebdbc48e6c9853b4acd2cbc2
12613 <1261337209@qq.com>
2021-07-13 22:14:55 +0800

    aa

--- algorithm/exam/T3.cpp
+++ algorithm/exam/T3.cpp
@@ -25,24 +25,24 @@ int maxSubArray(vector<int> &nums)
 }
 int main()
 {
-    // int n, j, sum, maxx;
-    // scanf("%d%d", &n, &maxx);
-    // sum = maxx; //输入n
-    // while (--n)
-    // {
-    //     scanf("%d", &j);
-    //     sum = sum > 0 ? sum : 0;
-    //     sum += j;
-    //     maxx = maxx > sum ? maxx : sum;
-    // }
-    // //贪心，如果负了就舍去
-    // printf("%d", maxx);
+    int n, j, sum, maxx;
+    scanf("%d%d", &n, &maxx);
+    sum = maxx; //输入n
+    while (--n)
+    {
+        scanf("%d", &j);
+        sum = sum > 0 ? sum : 0;
+        sum += j;
+        maxx = maxx > sum ? maxx : sum;
+    }
+    //贪心，如果负了就舍去
+    printf("%d", maxx);
 
-    vector<int> vec;
+    // vector<int> vec;
     
 
-    double a = floor(6.5678);
-    cout<<a<<endl;
+    // double a = floor(6.5678);
+    // cout<<a<<endl;
     return 0;
     //输出并return 0
 }
